1145db5937f39e6e77d880385a54c6a6382054c8
minasamy2000 <136934590+minasamy2000@users.noreply.github.com>
2023-08-11 18:33:39 +0300

    Add files via upload

---
+++ images/New folder/main.js
@@ -0,0 +1,82 @@
+ var home=document.getElementById("home1");
+ var whatdo=document.getElementById("whatdo1");
+ var skill=document.getElementById("skills1");
+ var goodat=document.getElementById("goodat1");
+ var contact=document.getElementById("contact1");
+ var feedback=document.getElementById("feedback1");
+ var overview=document.getElementById("overview1");
+ var home1=document.getElementById("home");
+ var whatdo1=document.getElementById("whatdo");
+ var skill1=document.getElementById("skills");
+ var goodat1=document.getElementById("goodat");
+ var contact1=document.getElementById("contact");
+ var feedback1=document.getElementById("feedback");
+ var overview1=document.getElementById("overview");
+home.onclick=function(){
+        home1.style.display="block"
+        skill1.style.display="none"
+        whatdo1.style.display="none"
+        contact1.style.display="none"
+        feedback1.style.display="none"
+        overview1.style.display="none"
+        goodat1.style.display="none"
+    }
+ whatdo.onclick=function(){
+    whatdo1.style.display="block"
+    skill1.style.display="none"
+    home1.style.display="none"
+    contact1.style.display="none"
+    feedback1.style.display="none"
+    overview1.style.display="none"
+    goodat1.style.display="none"  
+ }
+ skill.onclick=function(){  
+    skill1.style.display="block"
+     home1.style.display="none"
+     whatdo1.style.display="none"
+     contact1.style.display="none"
+     feedback1.style.display="none"
+     overview1.style.display="none"
+     goodat1.style.display="none"
+ }
+goodat.onclick=function(){
+        goodat1.style.display="block"
+        skill1.style.display="none"
+        home1.style.display="none"
+        whatdo1.style.display="none"
+        contact1.style.display="none"
+        feedback1.style.display="none"
+        overview1.style.display="none"
+        
+    }
+
+contact.onclick=function(){
+    contact1.style.display="block"
+    skill1.style.display="none"
+    home1.style.display="none"
+    whatdo1.style.display="none"
+    feedback1.style.display="none"
+    overview1.style.display="none"
+    goodat1.style.display="none"
+ }
+
+
+feedback.onclick=function(){
+    feedback1.style.display="block"
+    skill1.style.display="none"
+    home1.style.display="none"
+    whatdo1.style.display="none"
+    contact1.style.display="none"
+    overview1.style.display="none"
+    goodat1.style.display="none"
+ }
+overview.onclick=function(){
+        overview1.style.display="block"
+        skill1.style.display="none"
+        home1.style.display="none"
+        whatdo1.style.display="none"
+        contact1.style.display="none"
+        feedback1.style.display="none"
+        goodat1.style.display="none"
+     }
+
